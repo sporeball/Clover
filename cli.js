655aaec42f642c1739d1106996c1772d4bd522a0
sporeball <sporeballdev@gmail.com>
2022-12-01 17:13:41 -0800

    friendlier cli

--- cli.js
+++ cli.js
@@ -2,6 +2,21 @@
 
 import parse from './index.js';
 import fs from 'fs';
+import colors from 'picocolors';
 
-const code = fs.readFileSync(process.argv[2], { encoding: 'utf-8' });
-parse(code);
+function cli () {
+  let code;
+  try {
+    code = fs.readFileSync(process.argv[2], { encoding: 'utf-8' });
+  } catch (e) {
+    throw new Error('file not found');
+  }
+  parse(code);
+}
+
+try {
+  cli();
+} catch (e) {
+  console.log(`${colors.red('e:')} ${e.message}`);
+  process.exit(1);
+}
