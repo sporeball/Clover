f845195140aa309516f78a90e4c55d8c72fe42cd
sporeball <sporeballdev@gmail.com>
2022-12-02 11:14:32 -0800

    more comments

--- commands.js
+++ commands.js
@@ -1,12 +1,26 @@
 import { assert, is, option } from './util.js';
 
+/**
+ * each command written in a clover program consists of a list of tokens.
+ * if this list begins with a valid token, the interpreter will call a
+ * corresponding function, which runs code for that command if the list
+ * follows a valid pattern.
+ * all such functions take the entire list, and consume it one token at a time
+ * through callbacks.
+ */
+
+/**
+ * execute a command given the tokens
+ * @param {string[]} tokens
+ */
 export function evaluate (tokens) {
+  // look for a command pattern that starts with the head
   const head = tokens[0];
   if (!(head in commands)) {
     throw new Error(`no pattern for head token ${head}`);
   }
-  tokens.shift();
-  commands[head](tokens);
+  tokens.shift(); // remove head
+  commands[head](tokens); // run
 }
 
 function split (tk) {
--- index.js
+++ index.js
@@ -2,7 +2,12 @@ import * as Command from './commands.js';
 
 import fs from 'fs';
 
+/**
+ * parse a clover program
+ * @param {string} code
+ */
 export default function parse (code) {
+  // implicit input
   let input;
   try {
     input = fs.readFileSync('input.txt', { encoding: 'utf-8' });
@@ -11,15 +16,20 @@ export default function parse (code) {
   }
   console.log(input);
 
+  // commands act on the focus value
+  // this is equivalent to the original input at first
   global.focus = input;
 
   code = code.split('\n')
-    .map(line => line.replace(/;.*/gm, '').trim());
+    .map(line => line.replace(/;.*/gm, '').trim()); // clean
 
   for (const line of code) {
+    // skip empty lines
     if (line.length === 0) {
       continue;
     }
+    // each line of code holds a single command
+    // tokenize and evaluate
     const tokens = line.split(/ +/);
     Command.evaluate(tokens);
   }
