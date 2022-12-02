e63a1b2235928b8fbf41bcc07fde32c39cc5ea1b
sporeball <sporeballdev@gmail.com>
2022-12-01 16:57:34 -0800

    implicit input

--- index.js
+++ index.js
@@ -1,3 +1,12 @@
+import fs from 'fs';
+
 export default function parse(code) {
+  let input;
+  try {
+    input = fs.readFileSync('input.txt', { encoding: 'utf-8' });
+  } catch (e) {
+    input = '';
+  }
+  console.log(input);
   console.log(code);
 }
