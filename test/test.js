663a56f57760421f8fba4a896e077eadb1980c14
sporeball <sporeballdev@gmail.com>
2022-12-05 23:06:33 -0800

    test suite!!!

---
+++ test/test.js
@@ -0,0 +1,26 @@
+import parse from '../src/index.js';
+import Tentamen from 'tentamen';
+
+let tentamen = new Tentamen({});
+tentamen.fn = () => parse(tentamen.input, { silent: true });
+
+tentamen.suite('tests');
+tentamen.add('add', 'focus 5\nadd 5', 10);
+tentamen.add('divide', 'focus 5\ndivide by 5', 1);
+tentamen.add('focus', 'focus 5', 5);
+tentamen.add('multiply', 'focus 5\nmultiply by 5', 25);
+tentamen.add(
+  'refocus',
+  `focus 5
+  add 5
+  refocus`,
+  5
+);
+tentamen.add(
+  'show',
+  `focus 5
+  show
+  focus 10`,
+  [5, 10]
+);
+tentamen.add('subtract', 'focus 5\nsubtract 5', 0);
