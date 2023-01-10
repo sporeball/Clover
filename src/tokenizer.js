f86d4bc08154a801aa0a4ecbfad0f697fa1417ca
sporeball <sporeballdev@gmail.com>
2023-01-10 11:24:28 -0800

    flat only

--- src/tokenizer.js
+++ src/tokenizer.js
@@ -14,76 +14,22 @@ class Token {
   }
 }
 
-/**
- * class representing a terminal value
- */
-class Expr {
-  /**
-   * @param {string|RegExp} pattern
-   */
-  constructor (type, pattern) {
-    this.type = type;
-    this.pattern = pattern;
-    this.prec = 0;
-  }
-}
-
-class Choice {
-  constructor (type, choices, obj) {
-    this.type = type;
-    this.choices = choices.map(choice => {
-      if (choice instanceof Function) {
-        return choice.bind(T)();
-      }
-      return choice;
-    });
-    this.prec = obj.prec || 0;
-  }
-}
-
-class Seq {
-  constructor (type, seq, obj) {
-    this.type = type;
-    this.seq = seq.map(item => {
-      if (item instanceof Function) {
-        return item.bind(T)();
-      }
-      return item;
-    });
-    this.prec = obj.prec || 0;
-  }
-}
-
-class Repeat {
-  constructor (repeat, times) {
-    this.repeat = repeat.bind(T)();
-    this.times = times;
-  }
-}
-
 const T = {
-  command: function() {
-    return new Seq(
-      'command',
-      [this.word, new Repeat(this.value)],
-      { prec: 2 }
-    );
-  },
-  value: function() {
-    return new Choice(
-      'value',
-      [this.number, this.string],
-      { prec: 1 }
-    );
-  },
-  number: () => new Expr('number', /^0|^[1-9]\d*/g),
-  string: () => new Expr('string', /^'.*?'/g),
-  whitespace: () => new Expr('whitespace', /^\s+/g),
-  openBracket: () => new Expr('openBracket', '['),
-  closeBracket: () => new Expr('closeBracket', ']'),
-  openParen: () => new Expr('openParen', '('),
-  closeParen: () => new Expr('closeParen', ')'),
-  word: () => new Expr('word', /[^ ]+/g)
+  number: /^0|^-?[1-9]\d*/g,
+  string: /^'.*?'/g,
+  boolean: /^true|^false/,
+  openParen: '(',
+  closeParen: ')',
+  openBracket: '[',
+  closeBracket: ']',
+  openAngle: '<',
+  closeAngle: '>',
+  at: '@',
+  colon: ':',
+  equals: '=',
+  newline: '\n',
+  whitespace: /^\s+/g,
+  identifier: /^[^\s]+/g
 };
 
 /**
@@ -93,58 +39,14 @@ const T = {
  * @param {Expr} expr
  * @returns {string|undefined}
  */
-function stringMatch (value, expr) {
-  if (typeof value !== 'string') {
-    return;
-  }
-  if (typeof expr.pattern === 'string') {
+function stringMatch (value, matcher) {
+  if (typeof matcher === 'string') {
     return (
-      value.match(new RegExp('^' + escape(expr.pattern), 'g')) || []
+      value.match(new RegExp('^' + escape(matcher), 'g')) || []
     )[0];
   }
-  if (expr.pattern instanceof RegExp) {
-    return (value.match(expr.pattern) || [])[0];
-  }
-}
-
-function tokenMatch (tokens, matcher) {
-  if (matcher instanceof Expr) {
-    // if (stringMatch(tokens[0].value, matcher)) {
-    if (tokens[0].type === matcher.type) {
-      return [tokens[0]];
-    }
-  }
-  if (matcher instanceof Choice) {
-    if (matcher.choices.find(choice => tokens[0].type === choice.type)) {
-      return [tokens[0]];
-    }
-  }
-  if (matcher instanceof Seq) {
-    const tokensCopy = [...tokens];
-    let length = 0;
-    for (const [index, seqItem] of matcher.seq.entries()) {
-      if (tokenMatch(tokensCopy, seqItem)) {
-        const matchLength = tokenMatch(tokensCopy, seqItem).length;
-        length += matchLength;
-        tokensCopy.splice(0, matchLength);
-      } else {
-        return undefined;
-      }
-    }
-    return tokens.slice(0, length);
-  }
-  if (matcher instanceof Repeat) {
-    if (tokens[0].type === matcher.repeat.type) {
-      const nonMatchingIndex = tokens
-        .findIndex(token => token.type !== matcher.repeat.type);
-      if (nonMatchingIndex === -1) {
-        return tokens;
-      }
-      return tokens.slice(
-        0,
-        tokens.findIndex(token => token.type !== matcher.repeat.type)
-      );
-    }
+  if (matcher instanceof RegExp) {
+    return (value.match(matcher) || [])[0];
   }
 }
 
@@ -153,78 +55,35 @@ function tokenMatch (tokens, matcher) {
  * @returns {Token[]}
  */
 export function tokenize (code) {
-  // the first step is to create a flat list.
   let tokens = [];
-  // create an object
-  const bound = Object.fromEntries(
-    // containing the token type definitions...
-    Object.entries(T)
-      .map(entry => {
-        // with values replaced by their return values (bound to T).
-        entry[1] = entry[1].bind(T)();
-        return entry;
-      })
-  );
   // while there is still code to tokenize...
   while (code.length > 0) {
-    // find the first terminal expression
-    const expr = Object.values(bound)
-      .filter(value => value instanceof Expr)
+    // find the first expression
+    const expr = Object.entries(T)
       // which the code matches.
-      .find(value => {
-        return stringMatch(code, value) !== undefined;
+      .find(entry => {
+        return stringMatch(code, entry[1]) !== undefined;
       })
     if (expr === undefined) {
       throw new Error('no matching token type found');
     }
+    const [type, matcher] = expr;
     // use that match to make a token...
-    const match = stringMatch(code, expr);
+    const match = stringMatch(code, matcher);
     tokens.push(
-      new Token(expr.type, match)
+      new Token(type, match)
     );
     // then remove it from the code.
     code = code.slice(match.length);
   }
   // remove whitespace tokens (not significant here).
   tokens = tokens.filter(token => token.type !== 'whitespace');
-
-  // the next step is to make structures from the flat list.
-  // start at a precedence of 1.
-  let prec = 1;
-  let startPosition = 0;
-  while (true) {
-    // stop if there are no rules with the current precedence
-    if (startPosition > tokens.length - 1) {
-      startPosition = 0;
-      prec++;
-    }
-    const now = Object.values(bound)
-      .filter(value => value.prec === prec);
-    if (now.length === 0) {
-      break;
-    }
-    const rule = now
-      .find(value => {
-        return tokenMatch(tokens.slice(startPosition), value) !== undefined;
-      });
-    if (rule === undefined) {
-      startPosition++;
-      continue;
-    }
-    const match = tokenMatch(tokens.slice(startPosition), rule);
-    tokens.splice(
-      startPosition,
-      match.length,
-      new Token(rule.type, match)
-    );
-    startPosition++;
-  }
   return tokens;
 }
 
 console.dir(
   tokenize(
-    `focus ['a' 'b']
+    `focus [[1 2 3] [4 5 6] 'a']
     sum 3`
   ),
   { depth: null }
